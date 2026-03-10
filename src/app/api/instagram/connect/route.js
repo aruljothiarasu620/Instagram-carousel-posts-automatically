8be6e4248cdbd98da4adb3717756d8c7d4368445
Arul Jothi <aruljothiarasu620@gmail.com>
2026-03-10 21:59:37 +0530

    Fix active scopes for developer testing

--- src/app/api/instagram/connect/route.js
+++ src/app/api/instagram/connect/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const clientId = process.env.INSTAGRAM_APP_ID;
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/instagram/callback`;
-  const scope = 'instagram_business_basic,instagram_business_content_publish,pages_show_list,pages_read_engagement';
+  const scope = 'instagram_basic,pages_show_list,pages_read_engagement';
   const oauthUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
 
   return NextResponse.redirect(oauthUrl);
